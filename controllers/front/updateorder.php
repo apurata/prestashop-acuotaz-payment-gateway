ebf75ea40dc9f5e2545c2d29d7af7aa718b584d8
Andy Ñaca Rodriguez <74984535+andyapurata@users.noreply.github.com>
2020-12-01 21:34:10 -0500

    check if the order has already been created

--- controllers/front/updateorder.php
+++ controllers/front/updateorder.php
@@ -43,43 +43,51 @@ class Ps_ApurataUpdateOrderModuleFrontController extends ModuleFrontController
         $total = (float)$cart->getOrderTotal(true, Cart::BOTH);
         switch ($event) {
             case 'onhold':
-                error_log("Creating order...");
-                $this->module->validateOrder($id_cart, Configuration::get('PS_OS_APURATA'), $total, $this->module->displayName, NULL, NULL, (int)$currency->id, false, $customer->secure_key);
+                $id_order = (int)Order::getIdByCartId($id_cart);
+                //check if the order has already been created
+                if($id_order==0)
+                    $validation=$this->module->validateOrder(
+                        $id_cart,
+                        Configuration::get('PS_OS_APURATA'),
+                        $total,
+                        $this->module->displayName,
+                        NULL,
+                        NULL,
+                        (int)$currency->id,
+                        false,
+                        $customer->secure_key
+                    );
                 //Tools::redirect('index.php?controller=order-confirmation&id_cart='.$id_cart.'&id_module='.$this->module->id.'&id_order='.$this->module->currentOrder.'&key='.$customer->secure_key);
-                error_log(http_response_code());
-                header('HTTP/1.1 200 OK');
-                error_log("------------------------------------------------------");
+                http_response_code(200);
                 exit;
             case 'validated':
-                error_log("Validated");
                 $new_order_state = 2;
                 break;
             case 'rejected':
-                error_log("Rejected");
                 $new_order_state = 8;
                 break;
             case 'canceled':
-                error_log("Canceled");
                 $new_order_state = 6;
                 break;
+            //--states not implemented--
+            case 'created':
+                http_response_code(200);
+                exit;
+            case 'approved':    
+                http_response_code(200);
+                exit;
+            case 'funded':    
+                http_response_code(200);
+                exit;
+            //--------------------------
             default:
-                error_log($event);
-                error_log("State no supported");
-                die('Unsupported event');
+                error_log("No soportado ".$event);
+                return;
         }
         $id_order = (int)Order::getIdByCartId($id_cart);
-        error_log("Get Order ID");
-        // $order = new Order($id_order);
-        // $order->setCurrentState($new_order_state);   Doesn't work for me
-
         $history = new OrderHistory();
-        error_log("Before change state");
         $history->changeIdOrderState($new_order_state, $id_order);
-        // $history->save(); // It's found?
-        error_log("After change state");
         http_response_code(200);
-        echo http_response_code();
-        error_log("------------------------------------------------------");
         exit;
 	}
 }
